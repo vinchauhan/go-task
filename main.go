2daa0b879cd9ebe41a8b04ef448ba9b217d9abfb
Vineet <chauhan.vineet@gmail.com>
2018-07-27 00:15:55 -0700

    for db package

--- main.go
+++ main.go
@@ -1,14 +1,8 @@
 package main
 
-import (
-	"path/filepath"
-
-	"github.com/mitchellh/go-homedir"
-)
-
 func main() {
 
-	home, _ := homedir.Dir()
-	dbPath := filepath.Join(home, "tasks.db")
-	err := db.Init(dbPath)
+	//home, _ := homedir.Dir()
+	//dbPath := filepath.Join(home, "tasks.db")
+	//err := db.Init(dbPath)
 }
